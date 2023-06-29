06acd611d49c156ec3ee31676639ea6178657ee4
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-06-29 12:28:36 +0000

    [Add] 27-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,15 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+    
 
   def index
   end
@@ -10,4 +19,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
