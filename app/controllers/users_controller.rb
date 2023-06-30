73e4751c7ec5740055cfd05a61497bad9469091d
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-06-30 09:15:23 +0000

    [Add] 27-15

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    @books = @user.books
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
