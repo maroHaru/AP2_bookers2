75d6f61a9ecc5f52bad0340674b39059a878a2b9
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-07-08 05:49:55 +0000

    [Update] 27-Fixing RSpec test errors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -54,7 +54,7 @@ class BooksController < ApplicationController
   def is_matching_login_user
     @book = Book.find(params[:id])
     unless @book.user.id == current_user.id
-      redirect_to user_path(current_user.id)
+      redirect_to books_path
     end
   end
 
